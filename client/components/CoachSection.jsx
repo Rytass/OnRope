72374551b3d9d1c52a7e32eab5351e2e626036ab
Ting <ting@rytass.com>
2018-11-14 13:53:31 +0800

    create Avatar component

--- client/components/CoachSection.jsx
+++ client/components/CoachSection.jsx
@@ -1,6 +1,8 @@
 // @flow
 
 import React from 'react';
+import Avatar from './elements/Avatar.jsx';
+import avatar2 from '../static/images/avatar-02.png';
 
 const styles = {
   wrapper: {
@@ -17,12 +19,55 @@ const styles = {
     letterSpacing: 1.5,
     color: '#2fa3c4',
   },
+  sectionWrapper: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  main: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    borderWidth: '1px 0',
+    borderStyle: 'solid',
+    borderColor: '#398297',
+  },
+  contentText: {
+    fontSize: 14,
+    lineHeight: '24px',
+    letterSpacing: 1,
+    color: 'rgba(255, 255, 255, 0.87)',
+    margin: 0,
+  },
 };
 
 function CoachSection() {
   return (
     <div style={styles.wrapper}>
       <h2 style={styles.title}>教練介紹</h2>
+      <div style={styles.sectionWrapper}>
+        <div style={styles.main}>
+          <Avatar
+            avatar={avatar2}
+            name="黃元君"
+            position="資深教練"
+            experience={() => (
+              <div>
+                <p style={styles.contentText}>2011-2015 亞陞國際繩索技術服務 教育訓練</p>
+                <p style={styles.contentText}>2016-目前-就職於千里達企業有限公司   安全經理</p>
+                <p style={styles.contentText}>2018-目前 TAIA臺灣廣告工程職業安全促進會第一屆理事兼教育訓練召集人</p>
+              </div>
+            )}
+            license={() => (
+              <div>
+                <p style={styles.contentText}>IRATA L3, 填縫系防水施工技術士，建築物外牆安全檢查人員</p>
+              </div>
+            )} />
+        </div>
+      </div>
     </div>
   );
 }
---
+++ client/components/elements/Avatar.jsx
@@ -0,0 +1,91 @@
+// @flow
+
+import React from 'react';
+import radium from 'radium';
+
+const styles = {
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  avatarWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+  avatar: {
+    width: 180,
+    height: 180,
+  },
+  name: {
+    fontSize: 30,
+    lineHeight: 1.33,
+    letterSpacing: 1,
+    color: '#fff',
+    maring: '35px 0 20px 0',
+  },
+  position: {
+    fontSize: 18,
+    fontWeight: 600,
+    lineHeight: 2.22,
+    letterSpacing: 1,
+    color: '#4bb3d0',
+    margin: 0,
+  },
+  infoWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    margin: '0 0 0 40px',
+  },
+  infoTitle: {
+    fontSize: 14,
+    lineHeight: '24px',
+    letterSpacing: 1,
+    color: '#fff',
+    margin: 0,
+  },
+  infoContentWrapper: {
+    margin: '0 0 24px 0',
+  },
+};
+
+function Avatar({
+  avatar,
+  name,
+  position,
+  experience,
+  license,
+}: {
+  avatar: string,
+  name: string,
+  position: string,
+  experience: Function,
+  license: Function,
+}) {
+  return (
+    <div style={styles.wrapper}>
+      <div style={styles.avatarWrapper}>
+        <img src={avatar} alt="avatar" style={styles.avatar} />
+        <p style={styles.name}>{name}</p>
+        <p style={styles.position}>{position}</p>
+      </div>
+      <div style={styles.infoWrapper}>
+        <div style={styles.infoContentWrapper}>
+          <p style={styles.infoTitle}>經歷</p>
+          {experience()}
+        </div>
+        <div>
+          <p style={styles.infoTitle}>相關證照</p>
+          {license()}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default radium(Avatar);
