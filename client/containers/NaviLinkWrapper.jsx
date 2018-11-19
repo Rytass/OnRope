b14a33094923fcf18688ca572eb1ac48fec81f3c
Ting <ting@rytass.com>
2018-11-19 15:03:34 +0800

    finish NaviButton

--- client/containers/NaviLinkWrapper.jsx
+++ client/containers/NaviLinkWrapper.jsx
@@ -16,49 +16,63 @@ const styles = {
 };
 
 class NaviLinkWrapper extends PureComponent {
+  state = {
+    scrollY: 0,
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', () => {
+      this.setState({ scrollY: window.pageYOffset });
+    });
+  }
+
   render() {
+    const {
+      scrollY,
+    } = this.state;
+
     return (
       <div style={styles.wrapper}>
         <NaviButton
           text="首頁"
           targetHeight={0}
-          isActived={window.pageYOffset === 0} />
+          isActived={scrollY >= 0 && scrollY < 600} />
         <NaviButton
           text="創立緣由"
           targetHeight={600}
-          isActived={window.pageYOffset === 600} />
+          isActived={scrollY >= 600 && scrollY < 1040} />
         <NaviButton
           text="課程介紹"
           targetHeight={1040}
-          isActived={window.pageYOffset === 1040} />
+          isActived={scrollY >= 1040 && scrollY < 4390} />
         <NaviButton
           text="繩索垂直舞蹈"
           targetHeight={4390}
-          isActived={window.pageYOffset === 4390} />
+          isActived={scrollY >= 4390 && scrollY < 5980} />
         <NaviButton
           text="場地介紹"
           targetHeight={5980}
-          isActived={window.pageYOffset === 5980} />
+          isActived={scrollY >= 5980 && scrollY < 7353} />
         <NaviButton
           text="裝備介紹&代購商品"
           targetHeight={7353}
-          isActived={window.pageYOffset === 7353} />
+          isActived={scrollY >= 7353 && scrollY < 9809} />
         <NaviButton
           text="教練介紹"
           targetHeight={9809}
-          isActived={window.pageYOffset === 9809} />
+          isActived={scrollY >= 9809 && scrollY < 11737} />
         <NaviButton
           text="事件回顧"
           targetHeight={11737}
-          isActived={window.pageYOffset === 11737} />
+          isActived={scrollY >= 11737 && scrollY < 13221} />
         <NaviButton
           text="到府教學"
           targetHeight={13221}
-          isActived={window.pageYOffset === 13221} />
+          isActived={scrollY >= 13221 && scrollY < 13590} />
         <NaviButton
           text="場地位置"
           targetHeight={13590}
-          isActived={window.pageYOffset === 13590} />
+          isActived={scrollY >= 13590} />
       </div>
     );
   }
